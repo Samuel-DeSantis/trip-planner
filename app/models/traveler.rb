1e8380c053eeb1f589089fe047a00f4c54afbb1b
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-12-02 22:42:05 -0700

    Added trips taken and trips total cost to Users show page

--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -13,4 +13,27 @@ class Traveler < ApplicationRecord
         "#{trip_count} #{'trip'.pluralize(trip_count)}."
     end
 
+    def trips_taken
+        self.trips.collect { |trip| trip_taken?(trip.date) if !trip.date.nil? }.filter{ |date| date }.count
+    end
+
+    def trips_total
+        self.trips.collect { |trip| trip.cost }.filter { |cost| !cost.nil? }.reduce(:+)
+    end
+
+    private
+
+    def date_today
+        Time.zone.now.strftime('%m/%d/%Y')
+    end
+
+    def numberize(date)
+        month, day, year = date.split('/')
+        (year + month + day).to_i
+    end
+
+    def trip_taken?(trip_date)
+        numberize(date_today) > numberize(trip_date)
+    end
+
 end
